189b339c32c59977498ed02f9c140ac2725ee0cf
Glen Robson <glen.robson@gmail.com>
2018-07-04 17:41:06 +0100

    Adding a wrapper script to call the mixed content report. This can be called from travis or a rake file

---
+++ check_mixedcontent.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Running mixed content report"
+# e.g. arg could be _site for a jekyll site
+find $1 -name "*.html" | xargs phantomjs scripts/report-mixed-content.js ;
+echo "report finsihed."
