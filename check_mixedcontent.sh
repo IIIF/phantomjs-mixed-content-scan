3252d9c68c103daa0ea8e52cd4e6fb3cbc7d08f7
Glen Robson <glen.robson@gmail.com>
2018-09-07 15:03:56 +0100

    Allowing remote calls

--- check_mixedcontent.sh
+++ check_mixedcontent.sh
@@ -8,5 +8,5 @@ else
 fi
 echo "Running mixed content report"
 # e.g. arg could be _site for a jekyll site
-find $1 -name "*.html" | xargs phantomjs --debug=$debug --web-security=false --disk-cache=true --ignore-ssl-errors=false --load-images=true --output-encoding=utf-8  report-mixed-content.js ;
+find $1 -name "*.html" | xargs phantomjs --debug=$debug --local-to-remote-url-access=true --web-security=false --disk-cache=true --ignore-ssl-errors=false --load-images=true --output-encoding=utf-8  report-mixed-content.js ;
 echo "report finsihed."
