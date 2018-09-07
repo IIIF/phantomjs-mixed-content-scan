276d133ab2f0d0a8ca51a345daddece981bbc14d
Glen Robson <glen.robson@gmail.com>
2018-09-07 13:03:17 +0100

    Adding a debug mode

--- check_mixedcontent.sh
+++ check_mixedcontent.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ $# -eq 2 && $2 -eq "debug" ]];
+then
+    debug="true"
+else
+    debug="false"
+fi
 echo "Running mixed content report"
 # e.g. arg could be _site for a jekyll site
-find $1 -name "*.html" | xargs phantomjs --web-security=false --disk-cache=true --ignore-ssl-errors=false --load-images=true --output-encoding=utf-8  report-mixed-content.js ;
+find $1 -name "*.html" | xargs phantomjs --debug=$debug --web-security=false --disk-cache=true --ignore-ssl-errors=false --load-images=true --output-encoding=utf-8  report-mixed-content.js ;
 echo "report finsihed."
